90960238ea03b4d0839de900ed60016bbff07617
Levan Sanadiradze <levanisana8@gmail.com>
2020-04-14 13:07:22 +0400

    Initial commit

---
+++ Assets/Scripts/PersonScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonScript : MonoBehaviour
+{
+    private bool infected = false;
+    private int infectedPeopleNear = 0;
+
+    private SimulationManager SM;
+
+    private Rigidbody2D RB;
+    private GameObject InfectionSpace;
+
+    void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        InfectionSpace = transform.Find("InfectionSpace").gameObject;
+        InfectionSpace.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(!infected && infectedPeopleNear > 0)
+        {
+            float dChance = Mathf.Pow(1.0f - SM.infectionChance, Time.deltaTime); //1.0f - Mathf.Pow(SM.infectionChance, Time.deltaTime);
+            float stayHealthyChance = Mathf.Pow(dChance, infectedPeopleNear);
+            float rand = Random.Range(0.0f, 1.0f);
+
+            if(stayHealthyChance < rand)
+            {
+                setInfected();
+            }
+        }
+    }
+
+    public void initialData(SimulationManager sm)
+    {
+        SM = sm;
+        Vector2 dir = Random.insideUnitCircle.normalized;
+        
+        RB.velocity = dir * SM.personSpeedInMPS * SM.sizeScale;
+    }
+
+    public void setInfected()
+    {
+        infected = true;
+        transform.Find("PersonVisual").GetComponent<SpriteRenderer>().color = Color.red;
+        InfectionSpace.SetActive(true);
+        InfectionSpace.GetComponent<CircleCollider2D>().radius = SM.infectionRadiusInMeters * SM.sizeScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 8) // Layer 8: PlayerObstacle
+        {
+            if(other.gameObject.name == "Left" || other.gameObject.name == "Right")
+            {
+                Vector2 vel = RB.velocity;
+                vel.x *= -1f;
+                RB.velocity = vel;
+            }
+            else if(other.gameObject.name == "Top" || other.gameObject.name == "Bottom")
+            {
+                
+                Vector2 vel = RB.velocity;
+                vel.y *= -1f;
+                RB.velocity = vel;
+            }
+        }
+        else if(other.gameObject.layer == 10) // Layer 10: PersonInfectionSpace
+        {
+            if(infected) return;
+            infectedPeopleNear ++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 10) // Layer 10: PersonInfectionSpace
+        {
+            if(infected) return;
+            infectedPeopleNear --;
+        }
+    }
+}
---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+    public Object PersonPrefab;
+
+    [Range(0.5f, 100.0f)] public float timeScale = 1.0f;
+    [Range(0.1f, 10.0f)] public float sizeScale = 0.1f;
+
+    [Range(1, 5000)] public int numberOfPeople = 500;
+    public int initialInfectedPeople = 1;
+    
+    public float infectionRadiusInMeters = 2.0f;
+    public float infectionChance = 0.2f;
+
+    public float recoveryChance = 0.01f;
+
+    public float personSpeedInMPS = 5.0f;
+
+
+    private float personScale = 0f;
+    private float minPersonScale = 1f;
+    private float maxPersonScale = 5f;
+
+    private float XBorder = 75f;
+    private float YBorder = 40f;
+
+    private Transform PeopleContainer;
+
+    void Awake()
+    {
+        PeopleContainer = GameObject.Find("PeopleContainer").transform;
+
+        calculatePersonScale();
+
+        XBorder -= 0.5f * personScale;
+        YBorder -= 0.5f * personScale;
+
+
+        for(int i = 0; i < numberOfPeople; i++)
+        {
+           addAPerson();
+        }
+
+        initiallyInfectPeople();
+    }
+
+    private void initiallyInfectPeople()
+    {
+        for(int i = 0; i < initialInfectedPeople; i++)
+        {
+            PeopleContainer.GetChild(i).GetComponent<PersonScript>().setInfected();
+        }
+    }
+
+    void Update()
+    {
+        onTimeScaleUpdate();
+        onSizeScaleUpdate();
+        //onNumberOfPeopleUpdate();
+    }
+
+    private void calculatePersonScale()
+    {
+        personScale = Mathf.Max(sizeScale, minPersonScale);
+        personScale = Mathf.Min(personScale, maxPersonScale);
+    }
+
+    private void addAPerson()
+    {
+        Vector3 pos = new Vector3(Random.Range(-XBorder, XBorder), Random.Range(-YBorder, YBorder), 0f);
+
+        GameObject person = GameObject.Instantiate(PersonPrefab, pos, new Quaternion(0, 0, 0, 0), PeopleContainer) as GameObject;
+        person.transform.Find("PersonVisual").localScale = new Vector3(personScale, personScale, personScale);
+
+        PersonScript PS = person.GetComponent<PersonScript>();
+
+        PS.initialData(this);
+    }
+
+    private void onTimeScaleUpdate()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    private void onSizeScaleUpdate()
+    {
+        calculatePersonScale();
+        foreach(Transform t in PeopleContainer)
+        {
+            t.Find("PersonVisual").localScale = Vector3.one * personScale;
+        }
+    }
+
+    private void onNumberOfPeopleUpdate()
+    {
+        int diff = PeopleContainer.childCount - numberOfPeople;
+        if(diff > 0)
+        {
+            for(int i = 0; i < diff; i++)
+            {
+                GameObject.Destroy(PeopleContainer.GetChild(i).gameObject);
+            }
+        }
+        else if(diff < 0)
+        {
+            for(int i = 0; i < -diff; i++)
+            {
+                addAPerson();
+            }
+        }
+    }
+}
