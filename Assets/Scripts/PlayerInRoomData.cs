a2aaecb453551263b6c7bca4ce17cea8c555aac3
Levan Sanadiradze <levanisana8@gmail.com>
2020-04-15 15:02:50 +0400

    Created Player in room data class

---
+++ Assets/Scripts/PlayerInRoomData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInRoomData
+{
+    public PlayerInRoomData parentData = null;
+    public Vector3 perviousPosition = Vector3.zero;
+    public RoomScript currentRoomScript = null;
+    public bool hasParent = false;
+}
