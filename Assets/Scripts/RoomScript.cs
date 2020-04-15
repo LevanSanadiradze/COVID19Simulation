0cf294a24bf83e6ec8e41f16cd5cad56f46ea1f5
Levan Sanadiradze <levanisana8@gmail.com>
2020-04-15 15:02:31 +0400

    Added chance to enter and exit room, etc.

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class RoomScript : MonoBehaviour
 {
     public float roomInfectionChance = 0.02f;
+    public float chanceToEnterRoom = 0.1f;
+    public float chanceToLeaveRoom = 0.075f;
+
+    public Transform Arena;
+
+    public RoomScript[] RoomsToGo = new RoomScript[0];
 }
